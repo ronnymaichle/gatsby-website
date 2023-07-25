6c2ad39515ca021ba3ed8f5e3c1555654306aedc
Ronny <ronnymaichle@users.noreply.github.com>
2023-07-25 21:24:14 +0200

    final fix

---
+++ gatsby-ssr.js
@@ -0,0 +1,10 @@
+import "./src/styles/global.css";
+// import "bootstrap/dist/js/bootstrap.min.js";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+import React from "react";
+import { MenuProvider } from "./src/components/MenuContext";
+
+export const wrapRootElement = ({ element }) => (
+  <MenuProvider>{element}</MenuProvider>
+);
